e7911d0f3010b52c37abbe6f0d89ed2fb62fe308
UyDosh <uydoshtech@gmail.com>
2026-09-05 18:44:40 +0500

    Clarify expanded gender filter labels

--- assets/uydosh-i18n.js
+++ assets/uydosh-i18n.js
@@ -26,7 +26,7 @@
   var base = thisScript && thisScript.src
     ? thisScript.src.replace(/uydosh-i18n\.js.*$/, 'i18n/')
     : 'assets/i18n/';
-  var version = '20260829-13';
+  var version = '20260905-1';
   var canWrite = document.readyState === 'loading';
   langs.forEach(function (code) {
     var src = base + code + '.js?v=' + version;
